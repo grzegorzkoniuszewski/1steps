35fd2614221f452780ff05a20fe60a43a711cf29
grzegorzkoniuszewski <koniuszewski.grzegorz@gmail.com>
2024-10-13 11:14:49 +0200

    Modification First Class file.

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,5 +1,5 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("Hello Vistula in 2024 year");
+        System.out.println("Hello Vistula in 2024 year.");
     }
 }
